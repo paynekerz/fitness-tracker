8d138d0b6eb51d82276feed40b29d07dea3e00c4
Payne Kerz <paynekerz@gmail.com>
2021-08-04 05:45:55 -0700

    html routes

---
+++ routes/HTMLroutes.js
@@ -0,0 +1,10 @@
+const path = require("path")
+const router = require("express").Router()
+
+router.get("/exercise", (req, res)=>{
+    res.sendFile(path.join(__dirname, "../public/exercise.html"))
+});
+router.get("/stats", (req, res)=>{
+    res.sendFile(path.join(__dirname, "../public/stats.html"))
+});
+module.exports = router;
